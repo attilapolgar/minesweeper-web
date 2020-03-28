c9c82a190a02ea196811682655108d942e00f08b
Attila Polgar <raglopa@gmail.com>
2020-03-28 15:23:04 +0100

    chore(linter): a11y

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,17 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
+  plugins: ['jsx-a11y'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {
-    
-  },
+  rules: {},
   settings: {
     react: {
-      version: 'detect', 
+      version: 'detect',
     },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { ReactElement } from 'react'
 
 function App(): ReactElement {
-  return <div>🔥</div>
+  return (
+    <div>
+      <span role="img" aria-label="fire">
+        🔥
+      </span>
+    </div>
+  )
 }
 
 export default App
